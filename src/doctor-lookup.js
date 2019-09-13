11174621631e1173ad5c7d3eb1a6335931233074
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-13 10:28:17 -0700

    Add url for API call and initial UI for keyword search.

--- src/doctor-lookup.js
+++ src/doctor-lookup.js
@@ -1,6 +1,6 @@
-export function getDoctorInfo() {
+export function getDoctorInfo(query) {
   return new Promise(function(resolve, reject) {
-    let url = ``;
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
     let request = new XMLHttpRequest();
     request.onload = function() {
       if(this.status === 200) {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { getDoctorInfo } from './doctor-lookup.js';
 
 $(document).ready(function() {
-  
+  $("#doctor-lookup").submit(function(event) {
+    event.preventDefault();
+    let query = $("#query").val();
+    let doctorPromise = getDoctorInfo(query);
+    doctorPromise.then(function(response) {
+      const doctorInfo = JSON.parse(response);
+
+      console.log(doctorInfo);
+    });
+  });
 });
