8925d1f76a2d38baf12ac3633d31afaa04fe08a8
Jacqueline Remmel <jpremmel@gmail.com>
2019-09-13 15:53:02 -0700

    Add nested for loop to display all of each doctor's practices

--- src/main.js
+++ src/main.js
@@ -13,21 +13,37 @@ $(document).ready(function() {
       const doctorInfo = JSON.parse(response);
       console.log(doctorInfo);
 
-      for (let i = 0; i < doctorInfo.data[0].practices.length; i++) {
-        let address = `<br>${doctorInfo.data[0].practices[i].visit_address.street}<br>${doctorInfo.data[0].practices[i].visit_address.street2}<br>${doctorInfo.data[0].practices[i].visit_address.city}, ${doctorInfo.data[0].practices[i].visit_address.state} ${doctorInfo.data[0].practices[i].visit_address.zip}`;
-        let website;
-        if (doctorInfo.data[0].practices[i].website) {
-          website = doctorInfo.data[0].practices[i].website;
-        } else {
-          website = "none listed";
-        }
-        let acceptsNewPts;
-        if (doctorInfo.data[0].practices[i].accepts_new_patients) {
-          acceptsNewPts = "Yes";
-        } else {
-          acceptsNewPts = "No";
-        }
-        $(".output").append(`<div class=\"card\"><h4><span class=\"label\">Name (first and last): </span>${doctorInfo.data[0].practices[i].name}</h4><p><span class=\"label\">Phone number: </span>5032161150</p><p><span class=\"label\">Address:</span>${address}</p><p><span class=\"label\">Website: </span>${website}</p><p><span class=\"label\">Accepting New Patients: </span>${acceptsNewPts}</p></div>`);
+      for (let i = 0; i < doctorInfo.data.length; i++) {
+          let name = `${doctorInfo.data[i].profile.first_name} ${doctorInfo.data[i].profile.middle_name} ${doctorInfo.data[i].profile.last_name}, ${doctorInfo.data[i].profile.title}`;
+
+          let practices = [];
+
+          for (let j = 0; j < doctorInfo.data[i].practices.length; j++) {
+            let practiceName = doctorInfo.data[i].practices[j].name;
+            let practicePhone = doctorInfo.data[i].practices[j].phones[0].number;
+            let practiceAddress = `<br>${doctorInfo.data[i].practices[j].visit_address.street}<br>${doctorInfo.data[i].practices[j].visit_address.street2}<br>${doctorInfo.data[i].practices[j].visit_address.city}, ${doctorInfo.data[i].practices[j].visit_address.state} ${doctorInfo.data[i].practices[j].visit_address.zip}`;
+
+            let practiceWebsite;
+            if (doctorInfo.data[i].practices[j].website) {
+              practiceWebsite = doctorInfo.data[i].practices[j].website;
+            } else {
+              practiceWebsite = "No website listed";
+            }
+
+            let acceptsNewPts;
+            if (doctorInfo.data[i].practices[j].accepts_new_patients) {
+              acceptsNewPts = "Yes";
+            } else {
+              acceptsNewPts = "No";
+            }
+
+            practices.push(`<br><strong>${practiceName}</strong><br>${practicePhone}<br>${practiceAddress}<br>${practiceWebsite}<br>Accepting New Patients: ${acceptsNewPts}`);
+
+          }
+
+          let practiceNames = practices.join("<br> ");
+
+          $(".output").append(`<div class="card"><h4><span class="label">Name: </span>${name}</h4><p><span class="label">Practice(s): </span>${practiceNames}</p></div>`);
       }
 
     });
